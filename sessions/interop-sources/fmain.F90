aceec9f3fbe2ec3913e814754700573a14e43464
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2019-09-19 10:09:43 -0500

    fmain syntax cleanup

--- sessions/interop-sources/fmain.F90
+++ sessions/interop-sources/fmain.F90
@@ -1,12 +1,15 @@
 program finterop
+  implicit none
   external f 
-  integer f
-  integer i=5
+  integer :: f
+  integer :: i=5
   integer,dimension(3) :: array
   print *,f(i)
   array(1) = 20 ; array(2) = 21 ; array(3) = 22
   ! I want a function that zero's the ith element
+  ! in this case: zero the 2nd element
   call zero_i(array,2)
+  ! print out the whole array
   print *,array
 
 end program finterop
